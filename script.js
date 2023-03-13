cc5217595fe3d9eaf4aba645d00039bcd2439a3b
Mateusz-11 <tweak.night@gmail.com>
2023-03-13 21:51:25 +0100

    Added API configurations

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const input = document.querySelector('input')
-const button = document.querySelector('button')
-const cityName = document.querySelector('.city-name')
-const warning = document.querySelector('.warning')
-const photo = document.querySelector('.photo')
-const weather = document.querySelector('.weather')
-const temperature = document.querySelector('.temperature')
-const humidity = document.querySelector('.humidity')
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const cityName = document.querySelector(".city-name");
+const warning = document.querySelector(".warning");
+const photo = document.querySelector(".photo");
+const weather = document.querySelector(".weather");
+const temperature = document.querySelector(".temperature");
+const humidity = document.querySelector(".humidity");
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+const API_LINK = "https://api.openweathermap.org/data/2.5/weather?q=";
+const API_KEY = "&appid=";
+const API_UNITS = "&units=metric";
