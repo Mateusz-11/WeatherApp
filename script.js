9b4edcc44817d8ba52b85a0e8a6df68cd7297285
Mateusz-11 <tweak.night@gmail.com>
2023-03-16 20:15:47 +0100

    Added change  of weather pictures

--- script.js
+++ script.js
@@ -17,24 +17,39 @@ const getWeather = () => {
 	const city = input.value || "Warsaw";
 	const URL = API_LINK + city + API_KEY + API_UNITS;
 
-	axios.get(URL).then((res) => {
-		// console.log(res.data);
-		const temp = res.data.main.temp;
-		const hum = res.data.main.humidity;
-		const status = Object.assign({}, ...res.data.weather);
-		const pict = status.id;
-		console.log(status);
+	axios
+		.get(URL)
+		.then((res) => {
+			const temp = res.data.main.temp;
+			const hum = res.data.main.humidity;
+			const status = Object.assign({}, ...res.data.weather);
+			const pict = status.id;
 
-		cityName.textContent = res.data.name;
+			cityName.textContent = res.data.name;
 
-		temperature.textContent = `${Math.floor(temp)} °C`;
-		humidity.textContent = `${hum}%`;
+			temperature.textContent = `${Math.floor(temp)} °C`;
+			humidity.textContent = `${hum}%`;
 
-		// console.log(res.data.weather[0]);
-		weather.textContent = status.main;
+			// console.log(res.data.weather[0]);
+			weather.textContent = status.main;
 
-        if status.id 
-	});
+			if (status.id >= 200 && status.id < 300) {
+				photo.src = "img/thunderstorm.png";
+			} else if (status.id < 400) {
+				photo.src = "img/drizzle.png";
+			} else if (status.id < 600) {
+				photo.src = "img/rain.png";
+			} else if (status.id < 700) {
+				photo.src = "img/fog.png";
+			} else if (status.id === 800) {
+				photo.src = "img/sun.png";
+			} else if (status.id >= 800 && status.id < 900) {
+				photo.src = "img/cloud.png";
+			} else {
+				photo.src = "img/unknown.png";
+			}
+		})
+		.catch(() => (warning.textContent = "Wpisz popraną nazwę miasta"));
 };
 
 // getWeather();
